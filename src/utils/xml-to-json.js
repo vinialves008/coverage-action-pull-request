514dced0df48d4967118d027866dde53fa0565c3
vinialves008 <vinialves08@gmail.com>
2024-04-02 11:27:42 -0300

    teste 2

--- src/utils/xml-to-json.js
+++ src/utils/xml-to-json.js
@@ -1,4 +1,11 @@
+const xml2js = require('xml2js')
+
 const xmlToJSON = xml => {
+  const parser = new xml2js.Parser()
+  parser.parseString(xml, (err, result) => {
+    if (err) console.log(err)
+    console.log(result)
+  })
   return {
     coverage: {
       project: { metrics: { statements: 10, coveredstatements: 10 } }
